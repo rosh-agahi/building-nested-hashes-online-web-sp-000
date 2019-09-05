18d47771cbd4f22ba03e66e542b5131fc33590c0
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 23:02:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+   :montague => {
+     #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
-      }, 
-    :capulet => {
-      #your key/value pairs here!
+      :hero_friends => []
+   },
+   :capulet => {
+     #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
-      }
-  }
-
+      :heroine_friends => []
+   }
+}
 end
