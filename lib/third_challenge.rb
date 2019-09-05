5656d6593bc62ed2f5b56630cc87924d60c6f3fc
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 23:05:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,13 +8,13 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
         #your key/value pairs here
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :statue => "alive"
         },
       :hero_friends => []
@@ -22,13 +22,19 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
-        },
+        :name => "Juliet",
+        :age => "15",
+        :statue => "alive"
       :heroine_friends => []
    }
   }
